a7a151867f4d405c1e94b92a58eadc15dfda900b
rzarviv <rzarviv@gmail.com>
2022-12-26 13:32:47 +0000

    fixed an issue when database is empty

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -7,7 +7,7 @@ const ItemList = (props) => {
     return <div className="py-4">Loading...</div>;
   }
 
-  if (props.items.length === 0) {
+  if (props.items.length === 0 && props.searchQuery.length !== 0) {
     return (
       <div id="empty" className="py-4 no-items" style={{ display: "grid", justifyItems: "center" }}>
         <i className="bi bi-emoji-frown-fill" style={{ fontSize: "200%" }}></i>
