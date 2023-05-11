b0225ac1cf9d52deace592c4db822132d3ea6e87
WilcoApp <hello@wilco.gg>
2023-05-11 17:03:09 +0000

    Implementing Passport.js in Node.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,11 +32,7 @@ var UserSchema = new mongoose.Schema(
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
-    salt: String,
-    isVerified: {
-      type: Boolean,
-      default: false
-    }
+    salt: String
   },
   { timestamps: true }
 );
@@ -89,8 +85,7 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     bio: this.bio,
     image:
       this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
-    following: user ? user.isFollowing(this._id) : false,
-    isVerified: this.isVerified
+    following: user ? user.isFollowing(this._id) : false
   };
 };
 
--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -54,13 +54,6 @@ const ItemPreview = (props) => {
               className="user-pic rounded-circle pr-1"
             />
           </Link>
-          {
-          item.seller.isVerified && 
-          <div style={{display: "inline"}}>
-            <img src={"verified_seller.svg"} alt="verified seller"/> 
-            <span>TOP SELLER</span>
-            </div>
-            }
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
           </button>
